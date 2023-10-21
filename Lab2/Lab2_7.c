1c43d4b9b1a09a67f648f9ed018f6074601b0e1b
MIDLART <145492479+MIDLART@users.noreply.github.com>
2023-10-21 21:54:41 +0300

    Add files via upload

---
+++ Lab2/Lab2_7.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <math.h>
+
+typedef enum {
+    ok,
+    invalid_arguments
+} status_code;
+
+double function_1 (double x) {
+    return x - sqrt(2);
+}
+
+double function_2 (double x) {
+    return sqrt(x) - 3;
+}
+
+double function_3 (double x) {
+    return 5 * x - 7;
+}
+
+status_code dichotomy (double *res, double a, double b, double EPS, double (*function)(double)) {
+    double c = 0;
+    while (fabs(b - a) > EPS) {
+        c = (b + a)/2;
+        if (function(c) < EPS) {
+            a = c;
+        } else {
+            b = c;
+        }
+    }
+    *res = c;
+
+    return ok;
+}
+
+int main () {
+    double res;
+    dichotomy(&res, -0.1, 2.0, 1E-7, function_1);
+    printf("%f\n", res);
+    dichotomy(&res, -10.0, 100.0, 1E-7, function_2);
+    printf("%f\n", res);
+    dichotomy(&res, 0.0, 10.0, 1E-7, function_3);
+    printf("%f\n", res);
+
+    return ok;
+}
