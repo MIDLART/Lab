6575955373feeb9d49b35666782188d489be539b
MIDLART <145492479+MIDLART@users.noreply.github.com>
2024-03-06 00:32:53 +0300

    Create bst.h

---
+++ MP_4/Lab4_9/map/bst.h
@@ -0,0 +1,24 @@
+typedef struct bst_node
+{
+    char* key;
+    struct bst_node* left;
+    struct bst_node* right;
+    Department* dep;
+} bst_node;
+
+typedef struct BST
+{
+    bst_node* root;
+} BST;
+
+
+status_code bst_construct(BST* tree);
+status_code bst_destruct(BST* tree);
+status_code bst_set_null(BST* tree);
+
+status_code bst_insert(BST* tree, const char* key, Department* dep);
+status_code bst_search_node(BST* tree, const char* key, bst_node** node);
+status_code bst_contains(BST* tree, const char* key, int* is_contained);
+status_code bst_get(BST* tree, const char* key, Department** dep);
+status_code bst_erase(BST* tree, const char* key);
+void print_tree(bst_node *node, int tab);
